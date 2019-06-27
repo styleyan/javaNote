4399df9ca0d7be9b2bb142e857fdd11ed00a7134
yanxiaofei <forget_love2161@126.com>
2019-06-28 01:11:32 +0800

    feat: 增加通过 DefaultAdvisorAutoProxyCreator  自动代理配置示例

---
+++ java/com/isyxf/aop/spring3/StudentDao.java
@@ -0,0 +1,7 @@
+package com.isyxf.aop.spring3;
+
+public interface StudentDao {
+    public void save();
+    public void delete();
+    public void update();
+}
---
+++ java/com/isyxf/aop/spring3/StudentDaoImpl.java
@@ -0,0 +1,18 @@
+package com.isyxf.aop.spring3;
+
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public void save() {
+        System.out.println("StudentDao,-----保存");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("StudentDao,-----删除");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("StudentDao,-----更新");
+    }
+}
