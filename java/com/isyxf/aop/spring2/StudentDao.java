c7fb614c70b7ffd2443e2b68c3dbbbe0c7d5274e
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-06-28 23:24:02 +0800

    feat: 初始化demo

--- java/com/isyxf/aop/spring2/StudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.isyxf.aop.spring2;
-
-public interface StudentDao {
-    public void save();
-    public void delete();
-    public void update();
-}
--- java/com/isyxf/aop/spring3/StudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.isyxf.aop.spring3;
-
-public interface StudentDao {
-    public void save();
-    public void delete();
-    public void update();
-}
